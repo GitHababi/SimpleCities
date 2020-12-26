399c223241dd8b753728e7b69f840a8dd48da502
GitHababi <76075036+GitHababi@users.noreply.github.com>
2020-12-26 09:46:10 -0800

    Added Files

---
+++ Assets/Scripts/CameraDragger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDragger : MonoBehaviour
+{
+	private float dist;
+	private Vector3 MouseStart;
+	private Vector3 derp;
+
+    bool cameramovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+		dist = transform.position.z;
+		Debug.Log("Cumslut");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+			if (Input.GetMouseButtonDown (1)) {
+			MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+			MouseStart = Camera.main.ScreenToWorldPoint (MouseStart);
+			MouseStart.z = transform.position.z;
+
+		} 
+		else if (Input.GetMouseButton (1)) {
+			var MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+			MouseMove = Camera.main.ScreenToWorldPoint (MouseMove);
+			MouseMove.z = transform.position.z;
+			transform.position = transform.position - (MouseMove - MouseStart);
+		}
+        
+    }
+}
