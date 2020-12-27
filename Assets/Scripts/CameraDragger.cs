15123a4fd1ceaf3729094d2ffc7c6c38375ee4a0
GitHababi <76075036+GitHababi@users.noreply.github.com>
2020-12-26 22:23:48 -0800

    Made some changes idk man
    fuck me

--- Assets/Scripts/CameraDragger.cs
+++ Assets/Scripts/CameraDragger.cs
@@ -1,14 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraDragger : MonoBehaviour
 {
+	public Logic logic;
+	
 	private float dist;
+	private Vector3 RangeVector;
 	private Vector3 MouseStart;
 	private Vector3 derp;
-
+	private Vector3 distance;
     bool cameramovement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,18 +24,22 @@ public class CameraDragger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-			if (Input.GetMouseButtonDown (1)) {
-			MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-			MouseStart = Camera.main.ScreenToWorldPoint (MouseStart);
-			MouseStart.z = transform.position.z;
+		
+			if (Input.GetMouseButtonDown(1)) {
+				MouseStart = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+				MouseStart = Camera.main.ScreenToWorldPoint(MouseStart);
+				MouseStart.z = transform.position.z;
 
-		} 
-		else if (Input.GetMouseButton (1)) {
-			var MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
-			MouseMove = Camera.main.ScreenToWorldPoint (MouseMove);
-			MouseMove.z = transform.position.z;
-			transform.position = transform.position - (MouseMove - MouseStart);
+			}
+			else if (Input.GetMouseButton(1)) {
+				var MouseMove = new Vector3(Input.mousePosition.x, Input.mousePosition.y, dist);
+				MouseMove = Camera.main.ScreenToWorldPoint(MouseMove);
+				MouseMove.z = transform.position.z;
+				transform.position = transform.position - (MouseMove - MouseStart);
+			}
+		if (Math.Abs(transform.position.x) >= Logic.rangeX || Math.Abs(transform.position.y) >= Logic.rangeY) {
+			RangeVector = new Vector3(Logic.rangeX, Logic.rangeY, 0);
+			transform.position = RangeVector;
 		}
-        
     }
 }
---
+++ Assets/Scripts/Logic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Logic : MonoBehaviour
+{
+    public static int rangeX = 20;
+    public static int rangeY = 20;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
